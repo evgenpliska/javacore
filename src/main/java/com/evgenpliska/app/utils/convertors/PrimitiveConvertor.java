01db87e0af165eedafa944a0f623aee7e16b1536
Evgen Pliska <shenjapliska@gmail.com>
2017-02-02 14:41:11 +0200

    Runner

--- src/main/java/com/evgenpliska/app/utils/convertors/PrimitiveConvertor.java
+++ src/main/java/com/evgenpliska/app/utils/convertors/PrimitiveConvertor.java
@@ -16,4 +16,20 @@ public class PrimitiveConvertor {
     public void floatToChar(){
         System.out.println("Input float value is "+ myFloat + ". Output char value is " + myChar);
     }
+    public char intToChar (int myInt){
+        return (char) myInt;
+    }
+
+    public void intToChar(){
+        System.out.println("Input int value is "+ myInt + ". Output char value is " + myChar);
+    }
+
+    public int charToInt(char myChar){
+        return (int) myChar;
+    }
+
+    public void charToInt(){
+        System.out.println("Input char value is "+ myChar + ". Output int value is " + myInt);
+    }
 }
+
--- src/main/java/com/evgenpliska/app/utils/runners/Lesson3Runner.java
+++ src/main/java/com/evgenpliska/app/utils/runners/Lesson3Runner.java
@@ -9,6 +9,8 @@ public class Lesson3Runner {
         public static void main (String args[]){
             PrimitiveConvertor convertor1 = new PrimitiveConvertor();
             convertor1.floatToChar();
+            convertor1.charToInt();
+            convertor1.intToChar();
         }
 }
 
